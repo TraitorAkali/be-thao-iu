7045e25c0c9d4fd1c4e5d4e23d2e113e7edf2e1d
Phạm Thạch Thanh Trúc <traitorakali@gmail.com>
2022-08-12 09:41:43 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Ế nhô bbi iu!",
+  text2: "Tui muốn hỏi bé cái này. Nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Mày có yêu tao không hả con kia ._.?",
+  text4: "Bé mà tắt đi tức là đi với thằng khác rồi nha :) ",
+  text5: "Không yêu chồng. Ghét chồng òi",
+  text6: "Có yêu chồng trúc",
+  text7: "Bé hết thương anh rồi",
+  text8: "Dỗ tui. Không là dỗi",
+  text9: "M có giải thích gì thì tao cũng yêu mày thôi",
+  text10: "Mãi yêu bé thảo của tui :>",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Nghĩ lễ tui xún 2 3 ngày xong dạy bé tiếng A á ớ cả ngày nha :>>>>",
+  text12: "Hông thèm",
 };
 
 $(document).ready(function () {
